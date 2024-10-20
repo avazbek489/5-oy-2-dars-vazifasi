949b74ea36c2e7a362c6f330274f16deec205e2f
avazbek489 <obidjonovavazbek787@gmail.com>
2024-10-20 22:32:14 +0500

    3-question end

--- js/script.js
+++ js/script.js
@@ -28,3 +28,52 @@
 // // // // Masalalar
 // // // Massivlarga oid
 // // ### Masala 1:
+// function lengthArrayVerify(arg) {
+//   let res = arg
+//     .filter(function (value) {
+//       return value.length >= 5;
+//     })
+//     .map(function (value) {
+//       return value.charAt(0).toUpperCase() + value.slice(1);
+//     });
+//   return res;
+// }
+// let arr = ["olma", "behi", "mercedes", "qulupnay", "Ferrari", "Windovs"];
+// let res1 = lengthArrayVerify(arr);
+// console.log(res1);
+
+// // ### Masala 2:
+// function student(arg) {
+//   let names = arg.map(function (student) {
+//     return student.name;
+//   });
+//   let res = arg.find(function (student) {
+//     return student.age > 20;
+//   });
+//   return res.name.toUpperCase() + " - " + res.age;
+// }
+// let arr = [
+//   { name: "Sarodr", age: 17 },
+//   { name: "Abdulloh", age: 16 },
+//   { name: "Murod", age: 29 },
+// ];
+// console.log(student(arr));
+
+// // // ### Masala 3:
+// function pricesSumArray(arg) {
+//   let sum = arg.filter(function (value) {
+//     return value.price > 10000;
+//   });
+//   let gen = sum.reduce(function (value, index) {
+//     return value + index.price;
+//   }, 0);
+
+//   return gen;
+// }
+// let arr = [
+//   { name: "Phone", price: 12000 },
+//   { name: "Tablet", price: 80000 },
+//   { name: "Laptop", price: 15000 },
+//   { name: "Monitor", price: 7000 },
+// ];
+// console.log(pricesSumArray(arr));
