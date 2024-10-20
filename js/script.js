d84eccbc8dc51f9ced867fda8581ae3466c30ba2
avazbek489 <obidjonovavazbek787@gmail.com>
2024-10-20 21:53:05 +0500

    Beginner the code

---
+++ js/script.js
@@ -0,0 +1,30 @@
+// var a = 1;
+// function outter() {
+//   var b = 2;
+//   function inner() {
+//     var c = 3;
+//     console.log(a);
+//     console.log(b);
+//     console.log(c);
+//   }
+//   inner();
+// }
+// outter();
+
+// console.log(x); // ?
+// var x = 5;
+// console.log(x); // ?
+
+// console.log(y); // ?
+// let y = 10;
+// console.log(y); // ?
+
+// {
+//   console.log(z); // ?
+//   let z = 15;
+//   console.log(z); // ?
+// }
+
+// // // // Masalalar
+// // // Massivlarga oid
+// // ### Masala 1:
